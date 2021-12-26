a613669e95dd7812a77fcfe0bb46b40317f07850
ISeePlusPlus <44550806+ISeePlusPlus@users.noreply.github.com>
2021-12-26 18:48:35 +0200

    add inital content to page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import LegendContainer from "./components/LegendContainer";
+import MainMenuContainer from "./components/MainMenuContainer";
+import SubMenu from "./components/SubMenu";
+import DropDownMenuContainer from "./components/DropDownMenuContainer";
+import data from "./data/Legends.json";
 
 function App() {
-  return <div className='App'></div>;
+  const { MonitorType, Legends, Monitor } = data;
+  return (
+    <>
+      <MainMenuContainer menuItems={MonitorType} />
+      <DropDownMenuContainer />
+      <SubMenu />
+      <LegendContainer />
+    </>
+  );
 }
 
 export default App;
--- src/components/DropDownMenuContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DropDownMenuContainer = () => {
-  return <div>dropDownTest</div>;
-};
-
-export default DropDownMenuContainer;
